9505c9261dd78a0297f1508d661b86f217d488ba
moominkhan0987 <khanmoomin695@gmail.com>
2025-11-27 23:59:24 +0530

    h

---
+++ wap_to_print_factorial_of_a_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n, i, fact = i;
+    scanf("%d", &n);
+        for (i=1; i<= n; i++)
+        fact *= i;
+         printf("facctorial=%d", fact);
+         return 0;
+    
+        }
+        
+        
+        
+
