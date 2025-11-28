49914f8f90b514d3ad41fb6b0faa48e5b9273d04
moominkhan0987 <khanmoomin695@gmail.com>
2025-11-28 13:00:59 +0530

    wap-to-read_the_matrix_and-sum_of_the_matrix

---
+++ WAP_TO_CHECK_ODD/wap_to _read_the-matrix-and-sum-of_the_matrix.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j, sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Read the matrix elements
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+
+    // Display the matrix
+    printf("\nThe matrix is:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Print the sum
+    printf("\nSum of all elements of the matrix = %d\n", sum);
+
+    return 0;
+}
